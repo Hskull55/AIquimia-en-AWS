b0f5354cca94aed9d4c30f0cc6b464ec96a50999
Hskull55 <hectorlasanta55@gmail.com>
2024-03-20 22:45:35 +0000

    Añadimos página de inicio y activamos la página de administración

--- LittleAIlchemy/LittleAIlchemy/urls.py
+++ LittleAIlchemy/LittleAIlchemy/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('prueba/', views.pagina_prueba, name='pagina_prueba'),
+    path('', views.inicio, name='inicio'),
     path('alquimia/', views.alquimia, name='alquimia'),
+    path('admin/', admin.site.urls),
 ]
 
 #urlpatterns = [
